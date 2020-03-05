681c358a1e1b2fbed6938a39322132830bbe5e33
Deepanshu Demla <deep.demla97@gmail.com>
2020-03-05 19:57:07 +0530

    Added constants

--- src/constants.js
+++ src/constants.js
@@ -3,9 +3,34 @@ const INVALID_COUNTRY_CODE = 'Please enter a valid Country Code'
 const INVALID_FIRST_NAME = 'Please enter a valid First Name'
 const INVALID_LAST_NAME = 'Please enter a valid Last Name'
 
+const INITIAL_CONTACTS = [
+  {
+    id: "111",
+    firstName: "Martin",
+    lastName: "Grill",
+    countryCode: "+1",
+    phone: "1234567890",
+  },
+  {
+    id: "222",
+    firstName: "John",
+    lastName: "Don",
+    countryCode: "+41",
+    phone: "9378628381",
+  },
+  {
+    id: "333",
+    firstName: "Ricky",
+    lastName: "Cage",
+    countryCode: "+12",
+    phone: "3245678902",
+  }
+]
+
 export {
   INVALID_PHONE,
   INVALID_COUNTRY_CODE,
   INVALID_FIRST_NAME,
-  INVALID_LAST_NAME
+  INVALID_LAST_NAME,
+  INITIAL_CONTACTS
 }
